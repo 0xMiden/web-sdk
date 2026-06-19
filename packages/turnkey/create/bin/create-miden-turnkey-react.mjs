197384d4e66b064b91b684ee5939ebe5eb99d341
Wiktor Starczewski <poszerny@gmail.com>
2026-06-19 13:54:07 +0200

    chore: bump miden SDK to 0.15.1 (#13)

--- packages/turnkey/create/bin/create-miden-turnkey-react.mjs
+++ packages/turnkey/create/bin/create-miden-turnkey-react.mjs
@@ -71,9 +71,9 @@ try {
 
   pkg.dependencies = {
     ...pkg.dependencies,
-    "@miden-sdk/miden-sdk": "^0.15.0",
-    "@miden-sdk/miden-turnkey": "^1.15.0",
-    "@miden-sdk/miden-turnkey-react": "^1.15.0",
+    "@miden-sdk/miden-sdk": "^0.15.1",
+    "@miden-sdk/miden-turnkey": "^1.15.1",
+    "@miden-sdk/miden-turnkey-react": "^1.15.1",
     "@turnkey/react-wallet-kit": "^1.6.2",
     buffer: "^6.0.3",
     process: "^0.11.10",
